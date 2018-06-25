c9ab345ea3b3608860f83bea4073bce43d453c5c
eggsvonsatan <elbadry.d@gmail.com>
2018-06-25 02:22:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,10 +18,9 @@ function whileLoop(number) {
 function doWhileLoop(array) {
   function maybeTrue() {
   return Math.random() >= 0.5
-
 }
 do {
-  array.pop()
+  array.pop();
 }
 while {
   (array.length > 0 && maybeTrue())
