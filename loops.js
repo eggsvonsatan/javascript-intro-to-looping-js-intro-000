c1c9a9442b6d07eae7b5e3f02b528a4f6fed599e
eggsvonsatan <elbadry.d@gmail.com>
2018-06-25 01:46:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,3 @@
-function forLoop ([]) {for ([let i = 1; i < 25; i++])
+function forLoop (array) {for (let i = 1; i < 25; i++)
 {if (i === 0) {return "I am a 1 strange loop"}; else {return `I am ${i} strange loops` }
 }
