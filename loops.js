0bb5ae6ce082d7d4f034fed644b4d6ce561ad473
eggsvonsatan <elbadry.d@gmail.com>
2018-06-25 02:04:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,8 +9,8 @@ function forLoop(array) {
   return array;
 }
 
-function whileLoop(n) {
-  let countdown = n;
+function whileLoop(4) {
+  let countdown = 4;
   while (countdown > 0) {console.log(--countdown)}
   return 'done';
 }
